b2155f8772dee78a81e33ac04549b871d83df1b5
sanjay <sanjay@mathisit.com>
2020-07-09 13:30:43 -0400

    Update of React Sheet

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react'
 import { CSVReader } from 'react-papaparse'
 import Table from './table'
 export default class CSVReader4 extends Component {
+  constructor(props) {
+    super(props);
+    // create a reference to child table component here and attach the reference when calling child component
+    this.tableElement = React.createRef();
+    this.tableElement_parallel = React.createRef();
+    this.tableElement_parallel_variation = React.createRef();
+
+    this.state = {
+      cards: [],
+    };
+  }
   handleOnDrop = (data) => {
     const Year=data[0].data[1];
     const Set=data[1].data[1];
@@ -13,16 +24,17 @@ export default class CSVReader4 extends Component {
     const cards=[];
     const parallel_sets=[];
     const parallel_variation=[];
+    cards.push([{value:""},{value:"Player"},{value:"Team"}])
     for (i = 4; i < data.length; i++) {
       if(data[i].data[0]!==""){
       if (data[i].data[2]==="") {
-        cards.push([count,data[i].data[1],data[i].data[3]]);
+        cards.push([{value:count},{value:data[i].data[1]},{value:data[i].data[3]}]);
         count=count+1;
       }
       else{
-        cards.push([count,data[i].data[1],data[i].data[3]]);
+        cards.push([{value:count},{value:data[i].data[1]},{value:data[i].data[3]}]);
         count=count+1;
-        cards.push([count,data[i].data[1],data[i].data[3],data[i].data[4]]);
+        cards.push([{value:count},{value:data[i].data[1]},{value:data[i].data[3]},{value:data[i].data[4]}]);
         count=count+1;
       }
     }
@@ -30,41 +42,52 @@ export default class CSVReader4 extends Component {
       break;
     }
   }
+  parallel_sets.push([{value:""},{value:"Parallel Sets"}])
   for(i=i+2;i<data.length;i++){
     if(data[i].data[0]!==""){
-    if(data[i].data[1]===""){
-    parallel_sets.push([parallel_count,data[i].data[0]]);
-    parallel_count=parallel_count+1;
+      if(data[i].data[1]===""){
+        parallel_sets.push([{value:parallel_count},{value:data[i].data[0]}]);
+        parallel_count=parallel_count+1;
+      }
+      else{
+        parallel_sets.push([{value:parallel_count},{value:data[i].data[0]},{value:data[i].data[1]}]);
+        parallel_count=parallel_count+1;
+      }
+    }
+    else{
+      break;
+    }
   }
-  else{
-    parallel_sets.push([parallel_count,data[i].data[0],data[i].data[1]]);
-    parallel_count=parallel_count+1;
-  }
-}
-else{
-  break;
-}
-}
-for(i=i+2;i<data.length;i++){
-  parallel_variation.push([parallel_variation_count,data[i].data[0]]);
-  parallel_variation_count=parallel_variation_count+1;
-}
-console.log(cards)
-console.log(parallel_sets)
-console.log(parallel_variation)
-}
+  parallel_variation.push([{value:""},{value:"Parallel Variations"}])
 
+  for(i=i+2;i<data.length;i++){
+    parallel_variation.push([{value:parallel_variation_count},{value:data[i].data[0]}]);
+    parallel_variation_count=parallel_variation_count+1;
+  }
+  console.log(cards)
+  console.log(parallel_sets)
+  console.log(parallel_variation)
+    this.setState({ cards });
+
+    // update child table component's state here once data has been parsed
+    this.tableElement.current.setState({grid:cards});
+    this.tableElement_parallel.current.setState({grid:parallel_sets});
+    this.tableElement_parallel_variation.current.setState({grid:parallel_variation});
+
+  }
   handleOnError = (err, file, inputElem, reason) => {
     console.log(err)
   }
-
   handleOnRemoveFile = (data) => {
     console.log('---------------------------')
+    this.tableElement.current.setState({grid:[]});
+    this.tableElement_parallel.current.setState({grid:[]});
+    this.tableElement_parallel_variation.current.setState({grid:[]});
+
     console.log(data)
     console.log('---------------------------')
   }
-
-  render() {
+  render(){
     return (
       <>
         <h5>Upload your files</h5>
@@ -75,10 +98,12 @@ console.log(parallel_variation)
           addRemoveButton
           onRemoveFile={this.handleOnRemoveFile}
         >
-
           <span>Click to upload.</span>
         </CSVReader>
-        <Table></Table>
+        <Table ref={this.tableElement}></Table>
+        <Table ref={this.tableElement_parallel}></Table>
+        <Table ref={this.tableElement_parallel_variation}></Table>
+
       </>
     )
   }
--- src/table.js
+++ src/table.js
@@ -1,30 +1,39 @@
 import _ from 'lodash';
 import React from 'react';
 import ReactDataSheet from 'react-datasheet';
-
 export default class Table extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      grid: [
-        [{ value: 1 }, { value: 3 }],
-        [{ value: 2 }, { value: 4 }],
-      ],
+      grid: [],
+      title:"",
+
+      // grid: [
+      //   [{ value: 1 }, { value: 3 }],
+      //   [{ value: 2 }, { value: 4 }],
+      // ],
     };
+    console.log("Constructor", this.state.grid)
+
   }
+  // function to update state of Table component
+
   render() {
     return (
-      <ReactDataSheet
-        data={this.state.grid}
-        valueRenderer={cell => cell.value}
-        onCellsChanged={changes => {
-          const grid = this.state.grid.map(row => [...row]);
-          changes.forEach(({ cell, row, col, value }) => {
-            grid[row][col] = { ...grid[row][col], value };
-          });
-          this.setState({ grid });
-        }}
-      />
+      <div>
+        <ReactDataSheet
+          data={this.state.grid}
+          valueRenderer={cell => cell.value}
+          onCellsChanged={changes => {
+            const grid = this.state.grid.map(row => [...row]);
+            changes.forEach(({ cell, row, col, value }) => {
+              grid[row][col] = { ...grid[row][col], value };
+            });
+            this.setState({ grid });
+          }}
+        />
+      </div>
     );
   }
 }
